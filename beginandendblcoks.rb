4abf3f75bbc60ae79baac1cfa1c2d891bd79935b
nramakrishna <rk.nadupuri@gmail.com>
2014-09-25 17:00:58 +0530

    begin and end blocks

---
+++ beginandendblcoks.rb
@@ -0,0 +1,14 @@
+
+# this is a comment
+puts "This is my statement"
+
+# begin block is executed first in the program
+# it is a top most execute statement
+BEGIN{
+   puts "this is my first statement"
+}
+
+# end statement executes at end of the file
+END{
+   puts "this is my last statement"
+}
